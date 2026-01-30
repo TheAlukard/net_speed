0e89cbbd029bdd8c2da9025385c746a8288a75d2
Alucard <fire0lord0zuko@proton.me>
2026-01-30 08:35:47 +0200

    .

--- net_speed.sh
+++ net_speed.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-count=64
+count=256
 # minimum is 8, because we have to subtract the 8 bytes that make up the header and 'ping' doesn't take a size less than 0
 psize=64
 # minimum is 2ms
-inter=10
+inter=2
 addr=google.com
 
 PACKET_SIZE=$(($psize - 8))
@@ -17,22 +17,25 @@ expr_received='([0-9]*[0-9])(?=.received)'
 expr_time='(?<=time.)([0-9]*[0-9])(?=ms)'
 expr_ploss='([0-9]*[0-9].?[0-9]*[0-9]|[0-9]*[0-9])(?=%)'
 
-out=$(ping -3 -f -i $INTERVAL -s $PACKET_SIZE -c $count $addr)
+out=$(ping -3 -i $INTERVAL -s $PACKET_SIZE -c $count $addr)
 
 transmitted=$(echo $out | grep -Po "$expr_transmitted")
 received=$(echo $out | grep -Po "$expr_received")
 time=$(echo $out | grep -Po "$expr_time")
 ploss=$(echo $out | grep -Po "$expr_ploss")
 bits=$(($received * $psize * 8))
-realtime=$(echo "($time - ($inter * ($count - 1))) / $count")
+realtime=$(echo "($time - ($inter * ($count - 1))) / $count" | bc -l)
 speed=$(echo "$bits / $realtime" | bc -l) # bits/ms
 KBps=$(echo "($speed * 1000) / 8 / 1024" | bc -l)
+MBps=$(echo "$KBps / 1024" | bc -l)
 
 echo $out
 echo "Trasmitted: $transmitted"
 echo "Received: $received"
 echo "Time: $time"
+echo "Real Time": $realtime
 echo "Packet Loss: $ploss"
 echo "Bits: $bits"
 echo "Speed: $speed"
 echo "KBps: $KBps"
+echo "MBps: $MBps"
