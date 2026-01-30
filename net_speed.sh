8a178247b57e4dc46686bc64220c2d36dab6660f
Alucard <fire0lord0zuko@proton.me>
2026-01-30 07:49:36 +0200

    Add packet loss regex and time regex

--- net_speed.sh
+++ net_speed.sh
@@ -1,25 +1,31 @@
 #!/bin/sh
 
-set -xe
+set -e
 
 count=256
 # minimum is 8, because we have to subtract the 8 bytes that make up the header and 'ping' doesn't take a size less than 0
-p_size=8
+psize=8
 # minimum is 2ms
 inter=10
 addr=google.com
 
-PACKET_SIZE=$(($p_size - 8))
+PACKET_SIZE=$(($psize - 8))
 INTERVAL=$(echo "$inter / 1000" | bc -l)
 
 expr_transmitted='([0-9]*[0-9])(?=.packets transmitted)'
 expr_received='([0-9]*[0-9])(?=.received)'
+expr_time='(?<=time.)([0-9]*[0-9])(?=ms)'
+expr_ploss='([0-9]*[0-9].?[0-9]*[0-9]|[0-9]*[0-9])(?=%)'
 
 out=$(ping -3 -f -i $INTERVAL -s $PACKET_SIZE -c $count $addr)
 
 transmitted=$(echo $out | grep -Po "$expr_transmitted")
 received=$(echo $out | grep -Po "$expr_received")
+time=$(echo $out | grep -Po "$expr_time")
+ploss=$(echo $out | grep -Po "$expr_ploss")
 
-
+echo $out
 echo "Trasmitted: $transmitted"
 echo "Received: $received"
+echo "Time: $time"
+echo "Packet Loss: $ploss"
