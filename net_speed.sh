9a1ab14fbe983d7334bafa3f74d7c9c351355bad
Alucard <fire0lord0zuko@proton.me>
2026-01-30 02:02:30 +0200

    Added regex for getting packets send and received

--- net_speed.sh
+++ net_speed.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+set -xe
 
 count=256
 # minimum is 8, because we have to subtract the 8 bytes that make up the header and 'ping' doesn't take a size less than 0
@@ -11,8 +12,12 @@ addr=google.com
 PACKET_SIZE=$(($p_size - 8))
 INTERVAL=$(echo "$inter / 1000" | bc -l)
 
-transmitted='(\\d+)(?=.packets transmitted)'
+expr_transmitted='([0-9]*[0-9])(?=.packets transmitted)'
+expr_received='([0-9]*[0-9])(?=.received)'
 
 out=$(ping -3 -f -i $INTERVAL -s $PACKET_SIZE -c $count $addr)
 
-echo $out
+transmitted=$(echo $out | grep -Po "$expr_transmitted")
+received=$(echo $out | grep -Po "$expr_received")
+
+
