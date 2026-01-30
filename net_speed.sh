d7ad48b63c171c3ce1635cbfc48603743c511af7
Alucard <fire0lord0zuko@proton.me>
2026-01-30 08:09:43 +0200

    Multiply Mb/ms by 1000 to get seconds

--- net_speed.sh
+++ net_speed.sh
@@ -6,7 +6,7 @@ count=256
 # minimum is 8, because we have to subtract the 8 bytes that make up the header and 'ping' doesn't take a size less than 0
 psize=8
 # minimum is 2ms
-inter=2
+inter=10
 addr=google.com
 
 PACKET_SIZE=$(($psize - 8))
@@ -24,7 +24,7 @@ received=$(echo $out | grep -Po "$expr_received")
 time=$(echo $out | grep -Po "$expr_time")
 ploss=$(echo $out | grep -Po "$expr_ploss")
 speed=$(echo "($received * $psize * 8) / ($time - ($inter * ($count - 1)))" | bc -l)
-MBps=$(echo "$speed / 1024" | bc -l)
+MBps=$(echo "$speed * 1000 / 1024" | bc -l)
 
 echo $out
 echo "Trasmitted: $transmitted"
